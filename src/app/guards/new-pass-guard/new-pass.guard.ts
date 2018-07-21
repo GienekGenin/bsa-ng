89ca70727be0db8e5747a932352bae7a57770213
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-07-21 20:32:11 +0200

    changes for today

--- src/app/guards/new-pass-guard/new-pass.guard.ts
+++ src/app/guards/new-pass-guard/new-pass.guard.ts
@@ -1,16 +1,16 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot} from '@angular/router';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewPassGuard implements CanActivate {
   state = false;
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<boolean>|Promise<boolean>|boolean {
+
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    console.log(route, state);
     return this.state;
   }
 }
