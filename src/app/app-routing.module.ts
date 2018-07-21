6eba6debd57f9f23259b827981d61c3a0e2ab70e
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-07-21 10:13:19 +0200

    added components and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {ContentComponent} from './content/content.component';
+import {ForgetPassComponent} from './forget-pass/forget-pass.component';
+import {LoginComponent} from './login/login.component';
+import {RegistrationComponent} from './registration/registration.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'content', component: ContentComponent},
+  {path: 'forget-pass', component: ForgetPassComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'registration', component: RegistrationComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
+import { ForgetPassComponent } from './forget-pass/forget-pass.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    LoginComponent,
+    ForgetPassComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
